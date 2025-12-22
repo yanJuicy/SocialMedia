e3faccfa01b8ca19f15a3192d1d65e7b0daec73e
pinkthengrey <hanjub3@gmail.com>
2025-12-22 17:32:26 +0900

    Create Title Component

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StatusBar, Text } from 'react-native';
-import { getFontFamily } from "./assets/fonts/helper";
+import { StatusBar } from 'react-native';
+import Title from './components/Title/Title';
 
 const App = () => {
   return (
     <SafeAreaView>
       <StatusBar barStyle="dark-content" />
-      <Text style={{ fontSize: 50, fontFamily: getFontFamily('Inter', '700') }}>
-        Hello World!
-      </Text>
+      <Title title={"Let's explore"} />
     </SafeAreaView>
   );
 };
