865077d0cf742d3737bd3f4a3eeac9b7d73a74db
pinkthengrey <hanjub3@gmail.com>
2025-12-23 18:23:01 +0900

    install FontAwesome Icons and header done

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StatusBar } from 'react-native';
+import { StatusBar, Text, TouchableOpacity, View } from 'react-native';
 import Title from './components/Title/Title';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import globalStyle from './assets/styles/globalStyle';
 
 const App = () => {
   return (
     <SafeAreaView>
       <StatusBar barStyle="dark-content" />
-      <Title title={"Let's explore"} />
+      <View style={globalStyle.header}>
+        <Title title={"Let's explore"} />
+        <TouchableOpacity style={globalStyle.messageIcon}>
+          <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'} />
+          <View style={globalStyle.messageNumberContainer}>
+            <Text style={globalStyle.messageNumber}>2</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
---
+++ assets/styles/globalStyle.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+import { getFontFamily } from '../fonts/helper';
+
+const globalStyle = StyleSheet.create({
+  header: {
+    marginLeft: 27,
+    marginRight: 17,
+    marginTop: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  messageIcon: {
+    padding: 14,
+    borderRadius: 100,
+    backgroundColor: '#F9FAFB',
+  },
+  messageNumberContainer: {
+    backgroundColor: '#F35BAC',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 10,
+    height: 10,
+    borderRadius: 10,
+    position: 'absolute',
+    right: 10,
+    top: 12,
+  },
+  messageNumber: {
+    color: '#FFFFFF',
+    fontSize: 6,
+    fontFamily: getFontFamily('Inter', '600'),
+  },
+});
+
+export default globalStyle;
